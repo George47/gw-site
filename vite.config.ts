b25457bb1c2a8643dec9fff0c4014903c585aa0c
George <george.siqi.wang@gmail.com>
2024-09-21 06:00:43 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/gw-site/',
   plugins: [
     remix({
+      basename: '/gw-site/',
       ssr: false,
       future: {
         v3_fetcherPersist: true,
